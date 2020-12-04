d77bbd5f88ac15815abc2cc5197e093d5f0e0011
Eric <lichao@kaike.la>
2020-12-05 07:38:13 +0800

    feat: 增加bin/cli

---
+++ bin/cli.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+'use strict';
+const execa = require('execa');
+const path = require('path');
+
+process.title = 'live-dev-server';
+
+execa('node', [path.resolve(__dirname, '../index.js')]).stdout.pipe(process.stdout);
