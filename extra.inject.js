84bdd9a73df450a8a2baf0403cf07ea65eb2cd22
Eric <lichao@kaike.la>
2020-12-04 15:28:28 +0800

    fix: 移除inject文件中的中文

--- extra.inject.js
+++ extra.inject.js
@@ -1,10 +1,9 @@
-// 文件示例说明
-if (msgData.indexOf('sign') < 0) return
-let data = JSON.parse(msgData)
+// example inject code
+if (msgData.indexOf('sign') < 0) return;
+let data = JSON.parse(msgData);
 if (data.sign == 'reload') {
-  window.location.reload()
+  window.location.reload();
 }
-
 if (data.sign == 'example') {
-  console.log('---- -----')
+  console.log('---- -----');
 }
