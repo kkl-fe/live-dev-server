01c1acff2cf43dd264b824fae454602d27cc19e9
Eric <lichao@kaike.la>
2020-12-04 13:23:50 +0800

    fix: inject不再存在默认的reload调用，通过默认文件注入

--- extra.inject.js
+++ extra.inject.js
@@ -1,4 +1,10 @@
 // 文件示例说明
+if (msgData.indexOf('sign') < 0) return
+let data = JSON.parse(msgData)
+if (data.sign == 'reload') {
+  window.location.reload()
+}
+
 if (data.sign == 'example') {
   console.log('---- -----')
 }
