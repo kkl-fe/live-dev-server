7dc3616f98349481ce7f1f96bcd3075d83f8ef32
Eric <lichao@kaike.la>
2020-12-11 12:22:15 +0800

    feat: 工作目录与服务目录默认保持一致性；

--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
-'use strict';
-const execa = require('execa');
-const path = require('path');
+'use strict'
+const execa = require('execa')
+const path = require('path')
 
-process.title = 'live-dev-server';
+process.title = 'live-dev-server'
 
-execa('node', [path.resolve(__dirname, '../lib/index.js')]).stdout.pipe(process.stdout);
+execa('node', [path.resolve(__dirname, '../lib/index.js')]).stdout.pipe(
+  process.stdout
+)
